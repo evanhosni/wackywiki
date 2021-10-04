6e0d55a5411acb8040ff496a819c7c76a307bf5c
evanhosni <evankhaledhosni@gmail.com>
2021-10-03 23:40:11 -0700

    basic html content and css layout

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,31 @@
-console.log('uwu :3')
+var nounArray = []
+var adjectiveArray = []
+var adverbArray = []
+var verbArray = []
+var pastTenseVerbArray = []
+
+var temp = document.getElementsByClassName('ball');
+document.onmousemove = (event) => {
+const x = (event.clientX * 100) / window.innerWidth + '%';
+const y = (event.clientY * 100) / window.innerHeight + '%';
+
+for (let i=0;i<2;i++){
+temp[i].style.left = x;
+temp[i].style.top = y;
+temp[i].transform = 'translate(-' + x + ',-' + y + ')';
+}
+};
+
+// function borderChange(){
+// var randomColor = Math.floor(Math.random()*16777215).toString(16);
+// document.getElementById("ball").style.borderColor = "#" + randomColor;
+// document.getElementById("ball2").style.borderColor = "#" + randomColor;
+// }
+
+// function changeColor(){
+// var randomColor = Math.floor(Math.random()*16777215).toString(16);
+// var randomColor2 = Math.floor(Math.random()*16777215).toString(16);
+// var element = document.getElementById("circle");
+// document.body.style.backgroundColor = "#" + randomColor2;
+// element.style.backgroundColor = "#" + randomColor;
+// }
